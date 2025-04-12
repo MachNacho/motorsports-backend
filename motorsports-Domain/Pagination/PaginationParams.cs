573ec909b226f7e76ecd603af7507a025491a1c6
Asi <64479526+MachNacho@users.noreply.github.com>
2025-04-12 15:30:20 +0200

    Add blob controller Add pagination params Add packages

---
+++ motorsports-Domain/Pagination/PaginationParams.cs
@@ -0,0 +1,14 @@
+﻿namespace motorsports_Domain.Pagination
+{
+    public class PaginationParams
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+
+        private const int MaxPageSize = 50;
+
+        public int Skip => (PageNumber - 1) * PageSize;
+
+        public int Take => PageSize > MaxPageSize ? MaxPageSize : PageSize;
+    }
+}
---
+++ motorsports-backend/Controllers/BlobController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using motorsports_Service.Contracts;
+
+namespace motorsports_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlobController : ControllerBase
+    {
+        private readonly IBlobService _blobService;
+        public BlobController(IBlobService blobService)
+        {
+            _blobService = blobService;
+        }
+        
+        [HttpGet("{fileName}")]
+        public async Task<IActionResult> Download(string fileName)
+        {
+            var (content, contentType) = await _blobService.GetFileAsync(fileName);
+            return File(content, contentType!, fileName);
+        }
+
+    }
+}
