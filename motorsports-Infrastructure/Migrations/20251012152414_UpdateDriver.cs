eed9ba1576e8587bea115b69136b7adfbce86aa4
Asi <64479526+MachNacho@users.noreply.github.com>
2025-10-14 20:11:02 +0200

    Create new migration

---
+++ motorsports-Infrastructure/Migrations/20251012152414_UpdateDriver.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace motorsports_Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDriver : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "Driver",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "Driver");
+        }
+    }
+}
