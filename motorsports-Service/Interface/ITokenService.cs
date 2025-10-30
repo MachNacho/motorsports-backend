958f19d41646b244fe2df114c9f5193f2ebdda0f
Asi <64479526+MachNacho@users.noreply.github.com>
2025-10-30 14:16:36 +0200

    Add new tests

--- motorsports-Service/Interface/ITokenService.cs
+++ motorsports-Service/Interface/ITokenService.cs
@@ -1,9 +1,16 @@
 ﻿using motorsports_Domain.Entities;
 
 namespace motorsports_Service.Interface
-{
+{    /// <summary>
+     /// Service responsible for creating and managing JWT tokens for user authentication
+     /// </summary>
     public interface ITokenService
-    {
-        string createToken(UserEntity user);
+    {    /// <summary>
+         /// Creates a JWT token for the authenticated user with role-based claims
+         /// </summary>
+         /// <param name="user">The user entity to create the token for</param>
+         /// <param name="roles">List of roles assigned to the user</param>
+         /// <returns>A signed JWT token string</returns>
+        string CreateToken(UserEntity user, IList<string>? roles);
     }
 }
